2998a87d281fbba9fbd7c85b50124d5d70a6607d
Udit Thacker <uditthacker1921@gmail.com>
2026-04-17 22:46:12 +0530

    Auth(07): added locking mechanism

--- src/main/java/com/udit/authlib/security/AuthService.java
+++ src/main/java/com/udit/authlib/security/AuthService.java
@@ -7,17 +7,24 @@ import com.udit.authlib.entity.Role;
 import com.udit.authlib.entity.User;
 import com.udit.authlib.enums.UserStatus;
 import com.udit.authlib.exception.UserAlreadyExistsException;
+import com.udit.authlib.exception.UserLockedException;
 import com.udit.authlib.repository.RoleRepository;
 import com.udit.authlib.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -31,8 +38,9 @@ public class AuthService {
   private final JwtUtils jwtUtils;
 
 
+  @Transactional
   public void registerUser(SignupRequest request) {
-    if(validateRequest(request)) {
+    if(!validateRequest(request)) {
       throw new UserAlreadyExistsException("Username or email already exists");
     }
     var encodedPassword = passwordEncoder.encode(request.getPassword());
@@ -49,18 +57,55 @@ public class AuthService {
     userRepository.save(userToSave);
   }
 
+  @Transactional
   public JwtResponse authenticateUser(LoginRequest request) {
-    Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
-    SecurityContextHolder.getContext().setAuthentication(authentication);
+    try {
+      Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+      SecurityContextHolder.getContext().setAuthentication(authentication);
 
-    String token = jwtUtils.generateJwtToken(authentication);
-    User user = (User) authentication.getPrincipal();
+      String token = jwtUtils.generateJwtToken(authentication);
+      User user = (User) authentication.getPrincipal();
 
-    return JwtResponse.builder()
-            .token(token)
-            .username(user.getUsername())
-            .roles(List.copyOf(user.getRoles()))
-            .build();
+      assert user != null;
+      user.setFailedLoginAttempts(0);
+      user.setLockedUntil(null);
+      userRepository.save(user);
+
+      return JwtResponse.builder()
+              .token(token)
+              .username(user.getUsername())
+              .roles(user.getRoles().stream().map(Role::getName).toList())
+              .build();
+    } catch (AuthenticationException e) {
+      if (e instanceof LockedException) {
+        throw new UserLockedException("Account is locked due to too many failed attempts");
+      }
+
+      String identifier = request.getUsername();
+      Optional<User> userOpt;
+      if (identifier.contains("@")) {
+        userOpt = userRepository.findUserByEmail(identifier);
+      } else {
+        userOpt = userRepository.findUserByUsername(identifier);
+      }
+
+      if (userOpt.isPresent()) {
+        User user = userOpt.get();
+        user.setFailedLoginAttempts(user.getFailedLoginAttempts() + 1);
+        if (user.getFailedLoginAttempts() >= 5) {
+          Calendar cal = Calendar.getInstance();
+          cal.add(Calendar.MINUTE, 60);
+          user.setLockedUntil(cal.getTime());
+          userRepository.save(user);
+          throw new UserLockedException("Too many failed attempts, your account has been locked. Please try again after some time");
+        } else {
+          userRepository.save(user);
+          throw new BadCredentialsException("Invalid username or password");
+        }
+      } else {
+        throw new BadCredentialsException("Invalid username or password");
+      }
+    }
   }
 
   private boolean validateRequest(SignupRequest request) {
